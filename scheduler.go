29e30456ffaae4cbad31df8df1b0b0f83238b7ad
Simon Tucker <simon@brotherlogic@gmail>
2018-11-25 01:10:03 -0800

    Adds merge step

--- scheduler.go
+++ scheduler.go
@@ -88,7 +88,9 @@ func (s *Scheduler) build(queEnt queueEntry, server string) (string, error) {
 	//Refresh the project
 	fetchCommand := &rCommand{command: exec.Command("git", "-C", s.dir+"/src/"+queEnt.job.GoPath, "fetch", "-p")}
 	s.runAndWait(fetchCommand)
-	s.log(fmt.Sprintf("BUILDING FETCH: %v and %v", fetchCommand.output, fetchCommand.erroutput))
+	mergeCommand := &rCommand{command: exec.Command("git", "-C", s.dir+"/src/"+queEnt.job.GoPath, "merge", "origin/master")}
+	s.runAndWait(mergeCommand)
+	s.log(fmt.Sprintf("BUILDING FETCH: %v and %v", mergeCommand.output, mergeCommand.erroutput))
 
 	buildCommand := &rCommand{command: exec.Command("go", "get", queEnt.job.GoPath)}
 	s.runAndWait(buildCommand)
