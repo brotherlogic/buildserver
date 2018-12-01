8a0dccadfb212a401502e726644eff743b4ca4b9
Simon Tucker <simon@brotherlogic@gmail>
2018-12-01 13:58:39 -0800

    Simpliied CLI

--- buildserver_cli/cli.go
+++ buildserver_cli/cli.go
@@ -43,8 +43,6 @@ func main() {
 		if err != nil {
 			log.Fatalf("Error on build: %v", err)
 		}
-
-		fmt.Printf("Response: %v\n", res)
 	case "latest":
 		res, err := client.GetVersions(ctx, &pb.VersionRequest{Job: &pbgbs.Job{Name: os.Args[2], GoPath: "github.com/brotherlogic/" + os.Args[2]}, JustLatest: true})
 		if err != nil {
@@ -52,7 +50,6 @@ func main() {
 		}
 
 		fmt.Printf("%v - %v\n", res.Versions[0].Version, time.Unix(res.Versions[0].VersionDate, 0))
-
 	case "crash":
 		file, err := ioutil.ReadFile(os.Args[4])
 		if err != nil {
