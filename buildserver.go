263860fff12fd10497e39daec2142bcb9d3df7c3
Simon Tucker <simon@brotherlogic@gmail>
2019-03-15 13:57:04 -0700

    Bumps threshold for long builds. This closes #318

--- buildserver.go
+++ buildserver.go
@@ -147,7 +147,7 @@ func (s *Server) dequeue(ctx context.Context) {
 				job := s.buildQueue[0]
 				s.currentBuilds++
 				s.buildQueue = s.buildQueue[1:]
-				if time.Now().Sub(job.timeIn) > time.Minute*10 {
+				if time.Now().Sub(job.timeIn) > time.Minute*30 {
 					s.RaiseIssue(ctx, "Long Build", fmt.Sprintf("%v took %v to get to the front of the queue (%v in the queue) %v", job.job.Name, time.Now().Sub(job.timeIn), job.queueSizeAtEntry, job.inFront[0]), false)
 				}
 				if len(s.blacklist) == 0 || s.blacklist[job.job.Name] {
