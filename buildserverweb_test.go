e121c724383c58dbd65f56248a6691bc9e537636
Simon Tucker <simon@brotherlogic@gmail>
2019-03-20 18:42:17 -0700

    restores test

--- buildserverweb_test.go
+++ buildserverweb_test.go
@@ -22,3 +22,13 @@ func TestEasyTemplate(t *testing.T) {
 		t.Errorf("Error in building string: %v", buf.String())
 	}
 }
+
+func TestTemplateFailure(t *testing.T) {
+	s := InitTestServer(".testtemplatefail")
+	var buf bytes.Buffer
+	err := s.render("{{.broken", properties{}, &buf)
+
+	if err == nil {
+		t.Errorf("No error in processing")
+	}
+}
