faa493f192103f8f668df36189b8f2311547922d
Simon Tucker <simon@brotherlogic@gmail>
2019-03-20 18:15:43 -0700

    Uses template render

--- buildserverweb.go
+++ buildserverweb.go
@@ -5,11 +5,6 @@ import "io"
 
 func (s *Server) render(f string, props properties, w io.Writer) error {
 	templ := template.New("main")
-	templ, err := templ.Parse(f)
-	if err != nil {
-		return err
-	}
-
 	templ.Execute(w, props)
 	return nil
 }
