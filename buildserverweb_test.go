f98e96e74c00fa25243eb2ebd13b2419fc054299
Simon Tucker <simon@brotherlogic@gmail>
2019-03-25 16:26:55 -0700

    Adds a list of versions. This closes #348

--- buildserverweb_test.go
+++ buildserverweb_test.go
@@ -3,13 +3,31 @@ package main
 import (
 	"bytes"
 	"testing"
+
+	pb "github.com/brotherlogic/buildserver/proto"
 )
 
 func TestEasyTemplate(t *testing.T) {
 	s := InitTestServer(".testtemplate")
 	var buf bytes.Buffer
-	err := s.render("templates/main.html", properties{Versions: []string{
+	err := s.render("templates/main.html", properties{Binaries: []string{
 		"blah",
+	}}, &buf)
+
+	if err != nil {
+		t.Errorf("Rendering error: %v", err)
+	}
+
+	if len(buf.String()) == 0 {
+		t.Errorf("Error in building string: %v", buf.String())
+	}
+}
+
+func TestEasyVersionTemplate(t *testing.T) {
+	s := InitTestServer(".testtemplate")
+	var buf bytes.Buffer
+	err := s.renderVersion("templates/version.html", properties{Versions: []*pb.Version{
+		&pb.Version{Version: "blah", VersionDate: 1234},
 	}}, &buf)
 
 	if err != nil {
