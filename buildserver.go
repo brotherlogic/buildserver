7e8b032b8d47eac8e326416dd556a27fb880b43b
Simon Tucker <simon@brotherlogic@gmail>
2019-06-04 19:07:48 -0700

    Tracking builds

--- buildserver.go
+++ buildserver.go
@@ -484,6 +484,14 @@ func (s *Server) serveUp(port int32) {
 	}
 }
 
+func (s *Server) aligner(ctx context.Context) error {
+	for _, job := range s.jobs {
+		s.Log(fmt.Sprintf("Aligning %v", job.Name))
+	}
+
+	return nil
+}
+
 func main() {
 	var quiet = flag.Bool("quiet", false, "Show all output")
 	flag.Parse()
@@ -505,6 +513,7 @@ func main() {
 	server.RegisterRepeatingTask(server.backgroundBuilder, "background_builder", time.Minute*5)
 	server.RegisterRepeatingTask(server.runCheck, "checker", time.Minute*5)
 	server.RegisterRepeatingTaskNonMaster(server.dequeue, "dequeue", time.Second)
+	server.RegisterRepeatingTaskNonMaster(server.aligner, "aligner", time.Minute)
 
 	server.preloadInfo()
 
