929a57165d466947cc24bd5d7a1d12ce048af854
simon <brotherlogic@gmail.com>
2021-11-01 16:07:41 -0700

    Ignore pkg files

--- buildserver.go
+++ buildserver.go
@@ -771,7 +771,7 @@ func (s *Server) runCleanup() {
 		if err != nil {
 			return err
 		}
-		if !info.IsDir() && !strings.HasSuffix(info.Name(), ".version") && strings.Contains(p1, "brotherlogic") {
+		if !info.IsDir() && !strings.HasSuffix(info.Name(), ".version") && strings.Contains(p1, "brotherlogic") && !strings.Contains(p1, "pkg") {
 			elems := strings.Split(p1, "/")
 			s.Log(fmt.Sprintf("%v -> %v (%v)", elems[6], p1, config.GetLatestVersions()[elems[6]].GetPath()))
 		}
